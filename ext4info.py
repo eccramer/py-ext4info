a59b4f36b241c043a2f57ec4e90aa2f8b1dbe605
Evan Cramer <eccramer@gmail.com>
2020-07-31 15:28:34 -0700

    Adding a lot more functions, and added labels for both the test and empty image.

--- ext4info.py
+++ ext4info.py
@@ -3,9 +3,36 @@
 import fire
 import struct
 import uuid
+import time
 
 BLOCKSIZE = 1024
 EXT4MAGIC = 0xEF53
+
+SB_ERROR_POLICY = {
+    1: 'Continue',
+    2: 'Remount read-only',
+    3: 'Panic at the Disco',
+}
+
+FS_CREATOR = {
+    0: 'Linux',
+    1: 'Hurd',
+    2: 'Masix',
+    3: 'FreeBSD',
+    4: 'Lites',
+
+}
+
+SB_REV = {
+    0: 'original',
+    1: 'V2',
+}
+
+STATE_MASKS = {
+    0x0001: 'cleanly_unmounted',
+    0x0002: 'errors_detected',
+    0x0004: 'orphans_being_recovered'
+}
 
 #struct size_of defines
 LE16 = '<H'
@@ -77,8 +104,62 @@ class Ext4Superblock:
     def get_inodes_per_group(self):
         return struct.unpack_from(LE32, self.superblock, 0x28)[0]
 
+    def __format_timestamp(self, timestamp):
+        if timestamp > 0:
+            return time.ctime(timestamp)
+        else:
+            return 'n/a'
+
     def get_time_last_mounted(self, raw=False):
         mtime = struct.unpack_from(LE32, self.superblock, 0x2C)[0]
+        
+        if raw:
+            return mtime 
+        else:
+            return self.__format_timestamp(mtime)
+
+    def get_time_last_write(self, raw=False):
+        wtime = struct.unpack_from(LE32, self.superblock, 0x30)[0]
+        
+        if raw:
+            return wtime 
+        else:
+            return self.__format_timestamp(wtime)
+
+    def get_mount_count(self):
+        return struct.unpack_from(LE32, self.superblock, 0x34)[0]
+
+    def __bitmask_flags(self, field, masks):
+        flags_present = []
+        for mask, description in masks.items():
+            if field & mask:
+                flags_present.append(description)
+        
+        return ' '.join(flags_present)
+
+    def get_superblock_state(self, raw=False):
+        state = struct.unpack_from(LE16, self.superblock, 0x3A)[0]
+        
+        if raw:
+            return state
+        else:
+            return self.__bitmask_flags(state, STATE_MASKS)
+
+    def get_error_policy(self, raw=False):
+        policy_raw = struct.unpack_from(LE16, self.superblock, 0x3C)[0]
+
+        if raw:
+            return policy_raw
+        else:
+            return SB_ERROR_POLICY[policy_raw]
+
+    def get_creator_os(self, raw=False):
+        creator_raw = struct.unpack_from(LE16, self.superblock, 0x3E)[0]
+
+        if raw:
+            return creator_raw
+        else:
+            return FS_CREATOR[creator_raw]
 
     def get_volume_uuid(self, raw=False):
         uuid_raw = self.superblock[0x68:0x78]
@@ -87,6 +168,22 @@ class Ext4Superblock:
             return uuid_raw
         else:
             return uuid.UUID(bytes=uuid_raw)
+
+    def get_journal_uuid(self, raw=False):
+        uuid_raw = self.superblock[0xD0:0xE0]
+
+        if raw:
+            return uuid_raw
+        else:
+            return uuid.UUID(bytes=uuid_raw)
+
+    def get_volume_label(self, raw=False):
+        label_raw = self.superblock[0x78:0x88]
+
+        if raw:
+            return label_raw
+        else:
+            return "".join(chr(c) for c in label_raw)
 
     def dump_raw_superblock(self):
         return self.superblock
@@ -109,6 +206,18 @@ def dumpe2fs_info(filename, raw=False):
     print('First Data Block: {}'.format(sb.get_first_data_block()))
 
     print('Block size: {}'.format(sb.get_block_size()))
+
+    print('Blocks per group: {}'.format(sb.get_blocks_per_group()))
+
+    print('Inodes per group: {}'.format(sb.get_inodes_per_group()))
+
+    print('Time last mounted: {}'.format(sb.get_time_last_mounted()))
+    
+    print('Time last write: {}'.format(sb.get_time_last_write()))
+
+    print('State Flags: {}'.format(sb.get_superblock_state()))
+
+    print('Volume Label: {}'.format(sb.get_volume_label()))
 
     print('Volume UUID: {}'.format(sb.get_volume_uuid()))
 
